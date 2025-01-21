e5818d933d6c09e370573976ebe6dfe034219f0c
renan freitas <rflins05@gmail.com>
2025-01-21 00:04:02 -0300

    Create project

---
+++ BIMM_assignment/BIMM_assignment/BIMM_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  BIMM_assignmentApp.swift
+//  BIMM_assignment
+//
+//  Created by Renan Freitas on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BIMM_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BIMM_assignment/BIMM_assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BIMM_assignment
+//
+//  Created by Renan Freitas on 20/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
